538b1d7d27df4d9677b77b8003ebad4b92662dde
KIM JAEHONG <jjaekong@gmail.com>
2018-10-12 10:23:00 +0900

    알럿 추가, 로딩 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,12 @@
     $(window).on("scroll", function() {
         ($(this).scrollTop() > 0)
             ? $('body').addClass('scrolled')
-            : $('body').removeClass('scrolled')
+            : $('body').removeClass('scrolled');
+    });
+    
+    $(window).on('load', function() {
+        $("html, body").removeClass("loading");
+        $("#loading").hide();
     });
     
 })(jQuery);
